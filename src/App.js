98988e1b67a4e2319e5349bf95b0d4a9a1de4313
BhargaviMupparaju <105601164+BhargaviMupparaju@users.noreply.github.com>
2024-02-26 20:38:54 -0500

    Adding front end

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonAppBar from "./components/Appbar";
+import Student from "./components/Student";
+function App() {
+	return (
+		<div className="App">
+			<ButtonAppBar />
+			<Student />
+		</div>
+	);
+}
+
+export default App;
